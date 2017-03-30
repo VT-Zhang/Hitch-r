00f933787076d2071fff13e5851271de9e74ccb7
VT-Zhang <jianz@vt.edu>
2017-03-30 14:20:21 -0400

    Flashing error/success w/ toast

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,6 @@ class SessionsController < ApplicationController
 
     def create
         user = User.find_by(email: params[:email])
-        puts "*******************"
-        puts user
-        puts "*******************"
         if user
             if user.authenticate(params[:password])
                 session[:user_id] = user.id
