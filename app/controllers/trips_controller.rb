ff812813a747f645168803e717f1c3b6bb6ef1e8
VT-Zhang <jianz@vt.edu>
2017-03-31 11:26:11 -0400

    Able to add Trips

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,7 +1,7 @@
 class TripsController < ApplicationController
     def create
         car = Car.find(params[:car_id])
-        trip = Trip.create(user: current_user, dStreetNum: params[:dStreetNum], dStreet: params[:dStreet], dCity: params[:dCity], dState: params[:dState], dZip: params[:dZip], aStreetNum: params[:aStreetNum], aStreet: params[:aStreet], aCity: params[:aCity], aState: params[:aState], aZip: params[:aZip], dDate: params[:dDate], aDate: params[:aDate], price: params:[price], car: car)
+        trip = Trip.create(user: current_user, dStreetNum: params[:dStreetNum], dStreet: params[:dStreet], dCity: params[:dCity], dState: params[:dState], dZip: params[:dZip], aStreetNum: params[:aStreetNum], aStreet: params[:aStreet], aCity: params[:aCity], aState: params[:aState], aZip: params[:aZip], dDate: params[:dDate], aDate: params[:aDate], price: params[:price], car: car)
         if trip.valid?
             flash[:success] = ["New trip created!"]
             redirect_to "/users/profile/#{session[:user_id]}"
