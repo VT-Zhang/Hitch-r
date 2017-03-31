afc346f12fd3cf3a60660301f89da387c204c528
Nishant Aggarwal <nish8192@gmail.com>
2017-03-31 14:43:47 +0000

    Minor bug fix adding cars

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,6 +1,6 @@
 class CarsController < ApplicationController
     def create
-        car = Car.create(user: current_user, make: params[:make], model: params[:model], year: params[:year], trim: params[:trim], VIN: params[:VIN], seats: params[:seats], mileage: params[:mileage], color: params[:color])
+        car = Car.create(user: current_user, make: params[:make], model: params[:model], style: params[:style], year: params[:year], trim: params[:trim], VIN: params[:VIN], seats: params[:seats], mileage: params[:mileage], color: params[:color])
         if car.valid?
             flash[:success] = ["New car added!!"]
             redirect_to "/users/profile/#{session[:user_id]}"
